e6f84bb80a39b02ee4a39c2d687bcf958a74470c
{ryosuke27} <opu.7455@gmail.com>
2021-05-12 22:27:24 +0900

    fix

--- laravel/resources/views/twitter.blade.php
+++ laravel/resources/views/twitter.blade.php
@@ -16,7 +16,7 @@
             <div class="media">
                 <img src="https://placehold.jp/70x70.png" class="rounded-circle mr-4">
                 <div class="media-body">
-                    <!-- <h5 class="d-inline mr-3"><strong>{{ $tweet->user->name }}</strong></h5> -->
+                    <h5 class="d-inline mr-3"><strong>{{ $tweet->user->name }}</strong></h5>
                     <h6 class="d-inline text-secondary">{{ date('Y/m/d', strtotime($tweet->created_at)) }}</h6>
                     <p class="mt-3 mb-0">{{ $tweet->text }}</p>
                 </div>
